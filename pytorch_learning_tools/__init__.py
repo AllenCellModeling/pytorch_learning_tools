b0b531ca53d60f5a2077e8854ba0d220961c7158
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2017-09-05 14:13:04 -0700

    began code reorganization

---
+++ pytorch_learning_tools/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import, division, print_function
+from .SimpleLogger import *
+from .imgToProjection import *
+from .model_utils import *
+from .train_model import *
+# from .version import __version__
