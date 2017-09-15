0fee67bb781142ef19a8d0c974af9e3ce421722e
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2017-09-15 13:26:11 -0700

    formatting for csvDataset

--- pytorch_learning_tools/data_providers/csvDataset.py
+++ pytorch_learning_tools/data_providers/csvDataset.py
@@ -9,14 +9,14 @@ from torchvision import transforms, utils
 
 
 def eight_bit_to_float(im, dtype=np.uint8):
-    imax = np.iinfo(dtype).max # imax = 255 for uint8
-    im = im/imax
-    return(im)
+    imax = np.iinfo(dtype).max  # imax = 255 for uint8
+    im = im / imax
+    return (im)
 
 
 def load_h5(h5_path, channel_inds):
     f = h5py.File(h5_path, 'r')
-    image = f['image'].value[channel_inds,::]
+    image = f['image'].value[channel_inds, ::]
     image = eight_bit_to_float(image)
     return image
 
@@ -24,7 +24,15 @@ def load_h5(h5_path, channel_inds):
 class csvDataset(Dataset):
     """csv Dataset."""
 
-    def __init__(self, csv_file_name, root_dir, data_path_col, target_col, unique_id_col, data_type='hdf5', channel_inds=(3,4,2), transform=None):
+    def __init__(self,
+                 csv_file_name,
+                 root_dir,
+                 data_path_col,
+                 target_col,
+                 unique_id_col,
+                 data_type='hdf5',
+                 channel_inds=(3, 4, 2),
+                 transform=None):
         """
         Args:
             csv_file_name (string): csv file with annotations, just the base name, not the full path.
@@ -42,7 +50,7 @@ class csvDataset(Dataset):
             transform (callable, optional): Optional transform to be applied on a sample.
         """
 
-        df = pd.read_csv(os.path.join(root_dir,csv_file_name))
+        df = pd.read_csv(os.path.join(root_dir, csv_file_name))
 
         # check that all files we need are present in the df
         good_rows = []
@@ -50,7 +58,7 @@ class csvDataset(Dataset):
             data_path = os.path.join(root_dir, df.ix[idx, data_path_col])
             if os.path.isfile(data_path):
                 good_rows += [idx]
-        print('dropping {0} rows out of {1} from {2}'.format(len(df)-len(good_rows), len(df), csv_file_name))
+        print('dropping {0} rows out of {1} from {2}'.format(len(df) - len(good_rows), len(df), csv_file_name))
         df = df.iloc[good_rows]
         df = df.reset_index()
 
@@ -77,9 +85,7 @@ class csvDataset(Dataset):
         target = self.df.ix[idx, self.target_col]
         unique_id = self.df.ix[idx, self.unique_id_col]
 
-        sample = {'data': data,
-                  'target': target,
-                  'unique_id':unique_id}
+        sample = {'data': data, 'target': target, 'unique_id': unique_id}
 
         if self.transform:
             sample = self.transform(sample)
