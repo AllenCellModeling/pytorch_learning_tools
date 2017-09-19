2bf2cd438251066c93103e9d05374258218ac3aa
Gregory Johnson <gregj@alleninstitute.org>
2017-09-19 11:04:49 -0700

    merge and minor bugfixes

--- pytorch_learning_tools/__init__.py
+++ pytorch_learning_tools/__init__.py
@@ -1,4 +1,8 @@
 from __future__ import absolute_import, division, print_function
+
+import matplotlib as mpl
+mpl.use('Agg')
+
 from .SimpleLogger import *
 from .imgToProjection import *
 from .model_utils import *
--- pytorch_learning_tools/train_model.py
+++ pytorch_learning_tools/train_model.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 import pickle
 import time
@@ -14,8 +16,8 @@ import torchvision.utils
 import torch.backends.cudnn as cudnn
 cudnn.benchmark = True
 
-from .SimpleLogger import SimpleLogger
-from .model_utils import set_gpu_recursive, load_model, save_state, save_progress, maybe_save
+from pytorch_learning_tools.SimpleLogger import SimpleLogger
+from pytorch_learning_tools.model_utils import set_gpu_recursive, load_model, save_state, save_progress, maybe_save
 
 import fire
 
@@ -122,5 +124,5 @@ def train_model(gpu_ids=0,
 # if called from the command line, args are `--argname argvalue` style.
 # call with no args for help/info.
 if __name__ == "__main__":
-    fire.Fire(check_csv_column_contents)
+    fire.Fire(train_model)
 
