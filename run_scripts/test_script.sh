6026fc7bbf5d00fa25fa3dc24dfad6ee4a959288
Gregory Johnson <gregj@alleninstitute.org>
2017-09-19 09:19:35 -0700

    improved command line script

--- run_scripts/test_script.sh
+++ run_scripts/test_script.sh
@@ -1,6 +1,11 @@
-cd ..
+cd ../
 
-python train_model.py \
+code_dir=$PWD"/pytorch_learning_tools"
+
+export PYTHONPATH=$code_dir:$PYTHONPATH
+export PATH=$code_dir:$PATH
+
+train_model.py \
     --gpu_ids 2 \
     --batch_size 50 \
     --nepochs 50 \
@@ -8,7 +13,7 @@ python train_model.py \
     --model_name conv3D \
     --data_save_path ./data/dp_h5.pyt \
     --save_dir ./output/classifier3D/ \
-    --train_module classifier_train \
+    --train_module train_single_target \
     --data_dir /root/results/ipp_dataset_cellnuc_seg_curated_8_24_17/ \
     --save_state_iter 1 \
     --save_progress_iter 1
