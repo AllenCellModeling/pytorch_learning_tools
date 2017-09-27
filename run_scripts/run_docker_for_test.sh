8f9215559ff9c2363060b39964c72f77a21ef169
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2017-09-27 13:51:00 -0700

    tweaked docker test script

--- run_scripts/run_docker_for_test.sh
+++ run_scripts/run_docker_for_test.sh
@@ -1,5 +1,9 @@
-nvidia-docker run    \
-                    -v $(dirname $(pwd -P)):/root/$(basename $(dirname $(pwd))) \
-                    -v /allen/aics/modeling/gregj/results:/root/results \
-                    gregj/pytorch_extended \
-                    bash -c 'cd pytorch_learning_tools/run_scripts; bash test_script.sh'
+#!/usr/bin/env bash
+
+PAR_DIR=$(dirname $(pwd -P))
+
+nvidia-docker run \
+    -v ${PAR_DIR}:/root/$(basename ${PAR_DIR}) \
+    -v /allen/aics/modeling/gregj/results:/root/results \
+    gregj/pytorch_extended \
+    bash -c 'cd pytorch_learning_tools/run_scripts; bash test_script.sh'
