a1716f5dcc871dab407aa525180794c7664903c7
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2017-09-27 14:20:13 -0700

    made docker test script more modualr

--- run_scripts/run_docker_for_test.sh
+++ run_scripts/run_docker_for_test.sh
@@ -5,5 +5,5 @@ PAR_DIR=$(dirname $(pwd -P))
 nvidia-docker run \
     -v ${PAR_DIR}:/root/$(basename ${PAR_DIR}) \
     -v /allen/aics/modeling/gregj/results:/root/results \
-    gregj/pytorch_extended \
+    ${USER}/pytorch_extended:master \
     bash -c 'cd pytorch_learning_tools/run_scripts; bash test_script.sh'
