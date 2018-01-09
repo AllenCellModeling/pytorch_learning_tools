a598ad9113c046ded9e5d90f93234ab15961405f
Gregory Johnson <gregj@alleninstitute.org>
2018-01-09 14:41:10 -0800

    added support for cuda devices

--- pytorch_learning_tools/utils/data_utils.py
+++ pytorch_learning_tools/utils/data_utils.py
@@ -1,12 +1,24 @@
 import torch
+import numpy as np
+
 
 def indices_to_onehot(indices, nclasses = None):
     if nclasses == None:
         nclasses = len(np.unique(indices))
-        
-    onehot = torch.FloatTensor(len(indices), nclasses)
-    onehot.zero_()
-    onehot.scatter_(1, indices, 1)
     
+    ndat = len(indices)
+    
+    is_cuda = False
+    if indices.is_cuda:
+        is_cuda = True
+        cuda_device = indices.get_device()
+    
+    onehot = torch.FloatTensor(ndat, nclasses)
+    onehot.zero_()
+    
+    if is_cuda:
+        onehot = onehot.cuda(cuda_device)
+    
+    onehot[np.arange(0, ndat), indices] = 1
+
     return onehot
-   
