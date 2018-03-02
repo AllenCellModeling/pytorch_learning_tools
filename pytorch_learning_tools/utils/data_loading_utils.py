29ebe0afd55a3effec93bd726ed55901f0c095a6
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2018-03-02 11:51:35 -0800

    fixed PIL to torch loading conversion

--- pytorch_learning_tools/utils/data_loading_utils.py
+++ pytorch_learning_tools/utils/data_loading_utils.py
@@ -11,7 +11,7 @@ def load_rgb_img(img_path, image_channels):
             if image_channels is not (0,1,2):
                 zero_channels = [i for i in (0,1,2) if i not in image_channels]
                 img_arr[:,:,zero_channels] = 0
-            return img_arr
+            return Image.fromarray(img_arr, mode='RGB')
 
 def load_greyscale_tiff(img_path):
     with open(img_path, 'rb') as f:
