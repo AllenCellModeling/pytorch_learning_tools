36034336cbd33dde84bd8c243b3a7206a41812fb
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2018-03-30 16:45:45 -0700

    finished dataset and dataprovider rewrite

---
+++ pytorch_learning_tools/data_providers/DataframeDataset.py
@@ -0,0 +1,96 @@
+import os
+from collections import Iterable
+from functools import partial
+
+import pandas as pd
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from torch.utils.data.dataloader import default_collate as collate
+
+
+class DataframeDataset(Dataset):
+    """general dataframe Dataset class"""
+
+    def __init__(self, df,
+                 tabularData={'outName1':'colName1', 'outName2':'colName2'},
+                 imageData={'image1':{'cols':['channel1colName', 'channel2colName']}, 'image2':{'cols':['RGBimagecolName']}}):
+        """
+        Args:
+            df (pandas.DataFrame): dataframe containing tabular data and/or the absolute paths to image locations
+            tabularData: dict whos values are column names in df whose entries will be returned for each data point, the names when returned will be the keys of this dict.
+                         Groups of colum names can be used for a value (eg generating an 'X' matrix) but should only be used for groups of numeric columns, not strings
+                         Columns should contain either strings or numeric types convertable to torch rensors (int, np.float, etc)
+            imageData: dict whose keys each specify the name of a returned image, and whose values specify how that image is loaded/constructed, via kewargs: 'cols', 'mode' 'transform', and 'aggregate'.
+                       'cols' (list) specifies which columns in the df contain paths to images that will be transformed/aggregated to construct an output images
+                       'mode' (string) specifies which mode PIL/Pillow load the image as, eg 'L' (greyscale), 'RGB', etc
+                       'aggregate' (function) specifies how each individual tensor from the transformed images will be aggregated into one image tensor, e.g. torch.stack, torch.cat, etc
+                       'transform' (function) specifies how aggregated image found in 'cols' will be transformed.
+                           mostly useful for one or three channel images using transforms.ToPILImage(), your transforms, and then transforms.ToTensor() inside transforms.Compose([])
+                           lambda transforms like transforms.Lambda(lambda x: mask*x) should work directly on tensors of any number of channels
+        """
+        self.opts = locals()
+        self.opts.pop('self')
+        self.opts.pop('df')
+        self.df = df.reset_index(drop=True)
+
+        for name,kwargs in self.opts['imageData'].items():
+            defaultkwargs = {'loader':partial(loadPILImages, mode='L'), 'aggregate':torch.stack, 'transform':transforms.Compose([])}
+            defaultkwargs.update(self.opts['imageData'][name]); self.opts['imageData'][name] = defaultkwargs
+
+    def __len__(self):
+        return len(self.df)
+
+    def _get_item(self, idx):
+        images = {name:kw['transform'](kw['aggregate'](kw['loader'](list(self.df.loc[idx,kw['cols']]))).squeeze(dim=1)) for name,kw in self.opts['imageData'].items()}
+        tabular = {name:self.df[cols].iloc[idx] for name,cols in self.opts['tabularData'].items()}
+        tabular = {name:(torch.from_numpy(value.values) if isinstance(value, pd.Series) else value) for name,value in tabular.items()}
+        return {**images, **tabular}
+
+    def __getitem__(self, idx):
+        return collate([self._get_item(i) for i in idx]) if isinstance(idx,Iterable) else self._get_item(idx)
+
+class DatasetFeatures(DataframeDataset):
+    def __init__(self, df, ycol='yCol', Xcols=['Xcol1', 'Xcol2', 'Xcol3']):
+        kwargs = locals()
+        tabularData={'X':kwargs['target'],
+                     'y':kwargs['target']},
+        DataframeDataset.__init__(self, df, tabularData=tabularData, imageData={})
+
+class DatasetH5ToTarget(DataframeDataset):
+    def __init__(self, df, target='targetCol', image='h5FilePathCol', channels=(3,2,4), imageTransform=transforms.Compose([])):
+        kwargs = locals()
+        tabularData={'target':kwargs['target']},
+        imageData={'image':{'cols':[kwargs['image']], 'loader':partial(loadH5images, channels=kwargs['channels']), 'transform':kwargs['imageTransform']}}
+        DataframeDataset.__init__(self, df, tabularData=tabularData, imageData=imageData)
+
+class DatasetSingleRGBImageToTarget(DataframeDataset):
+    def __init__(self, df, target='targetCol', image='imageFilePathCol', imageTransform=transforms.Compose([])):
+        kwargs = locals()
+        tabularData={'target':kwargs['target']}
+        imageData={'image':{'cols':[kwargs['image']], 'loader':partial(loadPILImages, mode='RGB'), 'transform':kwargs['imageTransform']}}
+        DataframeDataset.__init__(self, df, tabularData=tabularData, imageData=imageData)
+
+class DatasetSingleGreyScaleImagetoTarget(DataframeDataset):
+    def __init__(self, df, target='targetCol', image='imageFilePathCol', imageTransform=transforms.Compose([])):
+        kwargs = locals()
+        tabularData={'target':kwargs['target']}
+        imageData={'image':{'cols':[kwargs['image']], 'loader':partial(loadPILImages, mode='L'), 'transform':kwargs['imageTransform']}}
+        DataframeDataset.__init__(self, df, tabularData=tabularData, imageData=imageData)
+
+class DatasetMultipleGreyScaleImagestoTarget(DataframeDataset):
+    def __init__(self, df, target='targetCol', images=['imageFilePathCol1','imageFilePathCol2'], imageTransform=transforms.Compose([])):
+        kwargs = locals()
+        tabularData={'target':kwargs['target']}
+        imageData={'image':{'cols':kwargs['images'], 'loader':partial(loadPILImages, mode='L'), 'transform':kwargs['imageTransform']}}
+        DataframeDataset.__init__(self, df, tabularData=tabularData, imageData=imageData)
+
+class DatasetHPA(DataframeDataset):
+    def __init__(self, df, seqCol='antigenSequence', metadatacols=['EnsemblID','proteinID','antibodyName'], inputImageCols =['microtubuleChannel', 'nuclearChannel'], targetImageCol='antibodyChannel',
+                 inputImageTransform=transforms.Compose([]), targetImageTransform=transforms.Compose([])):
+        kwargs = locals()
+        tabularData={'sequence':kwargs['seqCol'], **dict(zip(metadatacols,metadatacols))}
+        imageData={'inputImage':{'cols':kwargs['inputImageCols'], 'loader':partial(loadPILImages, mode='L'), 'transform':kwargs['inputImageTransform']},
+                   'targetImage':{'cols':[kwargs['targetImageCol']], 'loader':partial(loadPILImages, mode='L'), 'transform':kwargs['targetImageTransform']}}
+        DataframeDataset.__init__(self, df, tabularData=tabularData, imageData=imageData)
--- pytorch_learning_tools/utils/data_loading_utils.py
+++ pytorch_learning_tools/utils/data_loading_utils.py
@@ -4,34 +4,21 @@ from PIL import Image
 import h5py
 from .data_utils import eight_bit_to_float
 
-def load_rgb_img(img_path, *args, **kwargs):
-    with open(img_path, 'rb') as f:
-        img = Image.open(f)
-        return img.convert('RGB')
 
-def load_greyscale_tiff(img_path, *args, **kwargs):
-    with open(img_path, 'rb') as f:
-        img = Image.open(f)
-        return img.convert('L')
-
-def load_h5(h5_path, image_channels, *args, **kwargs):
-    f = h5py.File(h5_path, 'r')
-    image = f['image'].value[image_channels, ::]
-    image = eight_bit_to_float(image)
-    return torch.from_numpy(image)
-
-def pick_image_loader(image_type):
-    if image_type in ['jpg','JPG','jpeg','JPEG','png','PNG','ppm','PPM','bmp','BMP']:
-        return load_rgb_img
-    elif image_type in ['tif', 'TIF', 'tiff', 'TIFF']:
-        return load_greyscale_tiff
-    else:
-        raise ValueError('image_type {} is not supported, only basic images'.format(image_type))
-
-def loadAndAggregateImages(paths, mode='L', aggregate=torch.stack, *args, **kwargs):
-    image_tensors = []
+def loadPILImages(paths, mode='L', *args, **kwargs):
+    tensors = []
     for path in paths:
         with open(path, 'rb') as f:
             img = Image.open(f)
-            image_tensors += [transforms.ToTensor()(img.convert(mode))]
-    return aggregate(image_tensors).squeeze(dim=1)
+            tensors += [transforms.ToTensor()(img.convert(mode))]
+    return tensors
+
+def loadH5images(paths, channels=(3,2,4), *args, **kwargs):
+    tensors = []
+    for path in paths:
+        f = h5py.File(path, 'r')
+        image = f['image'].value[channels, ::]
+        image = eight_bit_to_float(image)
+        tensors += [torch.from_numpy(image)]
+    return tensors
+
