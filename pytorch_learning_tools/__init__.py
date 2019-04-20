4949acc483fa567fb0cadcc84d2f7c5daba30d33
Rory Donovan <donovanr@alumni.reed.edu>
2019-04-20 12:51:57 -0700

    remove all promoted fns and classes from main init

--- pytorch_learning_tools/__init__.py
+++ pytorch_learning_tools/__init__.py
@@ -1,7 +1,3 @@
 from __future__ import absolute_import, division, print_function
 
-from .SimpleLogger import SimpleLogger
-from .imgToProjection import matproj, imgtoprojection
-from .utils.model_utils import init_opts, set_gpu_recursive, weights_init, load_model, maybe_save, save_progress, save_state
-from .train_model import train_model
-# from .version import __version__
+from .version import __version__
